bc4fa568d2f126ab872b15d613c889a4c78a2309
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2026-03-09 00:22:56 -0500

    Implement LED class in FelixTheCatLED namespace
    Added LED class for controlling an LED with various methods.

--- src/FelixTheCatLED.h
+++ src/FelixTheCatLED.h
@@ -1,6 +1,28 @@
 #ifndef FELIX_THE_CAT_LED_H
 #define FELIX_THE_CAT_LED_H
 
+#include <Arduino.h>
 
+namespace FelixTheCatLED {
+  class LED {
+    public:
+      LED(uint8_t pin);
+  
+      void begin();
+      void on();
+      void off();
+      void toggle();
+      bool isOn() const { return _state; }
+      void setPin(uint8_t pin);
+      void setActiveLow(bool activeLow);
+      bool isActiveLow() const { return _activeLow; }
+  
+    private:
+      uint8_t _pin;
+      bool _state;
+      bool _activeLow;
+  };
+
+} // namespace FelixTheCatLED
 
 #endif // FELIX_THE_CAT_LED_H
