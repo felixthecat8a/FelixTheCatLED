45ad2128b58aa612ccd3ce422ca58a6397bba953
Felix Ochoa <fo59paz@yahoo.com>
2026-05-09 21:11:07 -0500

    updated library

--- src/FelixTheCatLED.h
+++ src/FelixTheCatLED.h
@@ -1,28 +1,6 @@
 #ifndef FELIX_THE_CAT_LED_H
 #define FELIX_THE_CAT_LED_H
 
-#include <Arduino.h>
-
-namespace FelixTheCatLED {
-  class LED {
-    public:
-      LED(uint8_t pin);
-  
-      void begin();
-      void on();
-      void off();
-      void toggle();
-      bool isOn() const { return _state; }
-      void setPin(uint8_t pin);
-      void setActiveLow(bool activeLow);
-      bool isActiveLow() const { return _activeLow; }
-  
-    private:
-      uint8_t _pin;
-      bool _state;
-      bool _activeLow;
-  };
-
-} // namespace FelixTheCatLED
+#include "util/LED.h"
 
 #endif // FELIX_THE_CAT_LED_H
