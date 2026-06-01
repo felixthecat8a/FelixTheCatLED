6318e5dce19c749743463b4ad3c4a6e798f37657
Felix Ochoa <fo59paz@yahoo.com>
2026-05-31 22:30:03 -0500

    update example code for animations

--- examples/animations/animations.ino
+++ examples/animations/animations.ino
@@ -1,25 +1,24 @@
 #include <FelixTheCatLED.h>
-using namespace FelixTheCatLED;
 
 /* Blink Setup */
 #define LED_PIN 8
-LED blinkLED(LED_PIN);
-AnimBlinkLED<LED> animBlink(blinkLED, 2000);
+FelixTheCatLED::LED blinkLED(LED_PIN);
+FelixTheCatLED::AnimBlinkLED<FelixTheCatLED::LED> animBlink(blinkLED, 2000);
 
 /* Fade Setup: Use PWM pins denoted by a ~ symbol. */
 #define PWM_PIN 6
-PWM pwmLED(PWM_PIN);
-AnimFadeLED<PWM> animFade(pwmLED, 2000);
+FelixTheCatLED::PWM pwmLED(PWM_PIN);
+FelixTheCatLED::AnimFadeLED<FelixTheCatLED::PWM> animFade(pwmLED, 2000);
 
 /* RGB LED Setup: Use PWM pins denoted by a ~ symbol. */
 #define RGB_RED 9
 #define RGB_GREEN 10
 #define RGB_BLUE 11
-RGB rgbLED(RGB_RED, RGB_GREEN, RGB_BLUE);
+FelixTheCatLED::RGB rgbLED(RGB_RED, RGB_GREEN, RGB_BLUE);
 
 /* RGB LED colorwheel & fade setup */
-AnimColorwheel<RGB> colorwheel(rgbLED, 5000);
-AnimFadeLED<RGB> rgbFade(rgbLED, 2000, 5, 51);
+FelixTheCatLED::AnimColorwheel<FelixTheCatLED::RGB> colorwheel(rgbLED, 5000);
+FelixTheCatLED::AnimFadeLED<FelixTheCatLED::RGB> rgbFade(rgbLED, 2000, 5, 51);
 
 void setup() {
   blinkLED.begin();
